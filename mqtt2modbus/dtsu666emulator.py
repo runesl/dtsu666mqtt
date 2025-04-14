093aba8a1666ad22ad50750ad214de857da2a7b9
Rune Skou Larsen <1193502+runesl@users.noreply.github.com>
2025-04-14 10:16:23 +0200

    initial

---
+++ mqtt2modbus/dtsu666emulator.py
@@ -0,0 +1,119 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+import time
+import datetime
+import logging
+
+from pymodbus.payload import BinaryPayloadBuilder, BinaryPayloadDecoder
+from pymodbus.device import ModbusDeviceIdentification
+from pymodbus.version import version as ModbusVersion
+from pymodbus.constants import Endian
+
+#from pymodbus.server import StartSerialServer
+#from pymodbus.server.async import StartSerialServer
+from pymodbus.server.sync import StartSerialServer
+
+from pymodbus.transaction import ModbusRtuFramer
+from pymodbus.datastore import ModbusSequentialDataBlock, ModbusSparseDataBlock
+from pymodbus.datastore import ModbusSlaveContext, ModbusServerContext
+
+from threading import Thread
+from register_mapping import Registermapping
+
+wordorder = Endian.Big
+byteorder = Endian.Big
+
+header = [207, 701, 0, 0, 0, 0, 1, 10, 0, 0, 0, 1, 0, 0, 0, 1000, 0, 0, 1000, 0, 0, 1000, 0, 0, 1000, 1, 10, 0, 0, 0, 1000, 0, 0, 1000, 0, 0, 1000, 0, 0, 1000, 0, 0, 0, 0, 3, 3, 4]
+
+class dtsu666Emulator:
+	def __init__(self,
+		device,
+		SlaveID=0x01
+		):
+
+		self.threads = {}
+
+		# ----------------------------------------------------------------------- #
+		# initialize the server information
+		i1 = ModbusDeviceIdentification()
+		i1.VendorName = 'Pymodbus'
+		i1.ProductCode = 'PM'
+		i1.VendorUrl = 'http://github.com/riptideio/pymodbus/'
+		i1.ProductName = 'Pymodbus Server'
+		i1.ModelName = 'Pymodbus Server'
+#		i1.MajorMinorRevision = '1.5'
+		i1.MajorMinorRevision = ModbusVersion.short()
+
+
+#		self.RS485Port=device
+		self.RS485Settings = {
+			'port': device,
+			'baudrate': 9600,
+			'timeout': 0.005,
+			'stopbits': 1,
+			'bytesize': 8,
+			'parity': 'N',
+#			'identity': i1
+			}
+
+
+		self.block = ModbusSequentialDataBlock(0, [0]*0x4052)
+		# Add header:
+		self._setval(0, header)
+
+		self.store   = ModbusSlaveContext(hr=self.block)
+		self.context = ModbusServerContext(slaves={SlaveID: self.store}, single=False)
+
+	#------------------------------------------------
+	def _setval(self, addr, data):
+		self.block.setValues((addr+1), data)	# why +1 ?! ..ugly
+
+	#------------------------------------------------
+	def _startserver(self):
+		srv = StartSerialServer(context=self.context, framer=ModbusRtuFramer, **self.RS485Settings)
+
+		logging.info('Modbus server started')
+
+	#------------------------------------------------
+	def startserver(self):
+		self.threads['srv'] = Thread(target=self._startserver)
+		self.threads['srv'].start()
+
+	#------------------------------------------------
+	def update(self, data):
+		for k,v in data.items():
+			reg = Registermapping[k]["addr"]
+
+			d = v / Registermapping[k]["scale"]
+			builder = BinaryPayloadBuilder(byteorder=byteorder, wordorder=wordorder)
+			builder.add_32bit_float(d)
+
+			self._setval(reg, builder.to_registers())
+
+# ==========================================================================================
+# ==========================================================================================
+if __name__ == "__main__":
+
+#	RS485Port = '/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0' # Smartmeter
+#	RS485Port = "/dev/pts/7"
+	RS485Port = "/dev/serial0"
+
+	em1 = dtsu666Emulator(
+		device=RS485Port
+	)
+
+	em1.startserver()
+
+	Testdata = {'Volts_AB': 403.6, 'Volts_BC': 408.0, 'Volts_CA': 404.5, 'Volts_L1': 231.0, 'Volts_L2': 235.1, 'Volts_L3': 236.1, 'Current_L1': 0.339, 'Current_L2': 0.36, 'Current_L3': 0.352, 'Active_Power_L1': 2.8, 'Active_Power_L2': 11.8, 'Active_Power_L3': 8.5, 'Reactive_Power_L1': -76.7, 'Reactive_Power_L2': -80.0, 'Reactive_Power_L3': -79.7, 'Power_Factor_L1': 0.036, 'Power_Factor_L2': 0.14, 'Power_Factor_L3': 0.102, 'Total_System_Active_Power': 23.2, 'Total_System_Reactive_Power': -236.5, 'Total_System_Power_Factor': 0.094,}
+
+
+	while True:
+		logging.info("updating the context")
+		em1.update(Testdata)
+		time.sleep(10)
+
+	print("bla")
+
+
---
+++ mqtt2modbus/mqtt2smemulator.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from pprint import PrettyPrinter
+
+pp = PrettyPrinter(indent=4)
+
+import logging
+import logging.handlers as Handlers
+import os
+import time
+#log = logging.getLogger("pymodbus.server")
+
+log = logging.getLogger()
+log.setLevel(logging.INFO)
+#log.setLevel(logging.DEBUG)
+
+from dtsu666emulator import dtsu666Emulator
+import paho.mqtt.client as mqtt
+import json
+import math
+import sys
+import threading
+
+#RS485Port = "/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A50285BI-if00-port0" # Smartmeter-input on inverter
+RS485Port = "/dev/serial0" # Smartmeter-input on inverter
+
+
+MQTT_Settings = {
+	"Server":	"192.168.0.60",
+	"Port":     	1883,
+	"AMS_Topic":	"smartmeter",
+	}
+
+# Don't init until first message received
+em1 = ""
+last_received_timestamp = 0
+max_secs_delay_warn=3
+max_secs_delay_fail=10
+
+
+def watchdog_task():
+	logging.info("starting watchdog loop")
+	while True:
+		delay = time.time() - last_received_timestamp
+		if delay > max_secs_delay_warn:
+			log.warning(f"Delay is at {delay}")
+		if delay > max_secs_delay_fail:
+			log.warning(f"Delay is at {delay}. Exiting")
+			os._exit(1)
+		time.sleep(1)
+
+#============================================================================
+
+# The callback for when the client receives a CONNACK response from the server.
+def mqtt_on_connect(client, userdata, flags, rc):
+#	logging.info(f"MQTT connected with result code {rc}")
+
+	# Subscribing in on_connect() means that if we lose the connection and
+	# reconnect then subscriptions will be renewed.
+	client.subscribe(MQTT_Settings['AMS_Topic']+"/#")
+
+
+# The callback for when a PUBLISH message is received from the server.
+def mqtt_on_message(client, userdata, msg):
+#	log.info(f"got message! topic: {msg.topic}")
+	global last_received_timestamp
+	last_received_timestamp = time.time()
+#	print(msg.topic)
+
+	global em1
+
+	if em1 == "":
+		em1 = dtsu666Emulator(device=RS485Port)
+		em1.startserver()
+		logging.info('Got initial message. Smartmeter started...')
+		watchdog_thread = threading.Thread(target=watchdog_task, name="watchdog", daemon=True)
+		watchdog_thread.start()
+
+
+	if msg.topic == MQTT_Settings['AMS_Topic'] + "/power":
+		d = json.loads(msg.payload.decode("utf-8"))
+
+#		logging.info("..update power..")
+#		logging.debug(pp.pformat(d))
+		em1.update(d)
+
+
+	elif msg.topic == MQTT_Settings['AMS_Topic']+"/energy":
+		d = json.loads(msg.payload.decode("utf-8"))
+
+#		logging.info("..update energy..")
+#		logging.info(pp.pformat(d))
+		em1.update(d)
+
+
+# ========================================================================================================================
+mqttclient=mqtt.Client()
+mqttclient.on_connect = mqtt_on_connect
+mqttclient.on_message = mqtt_on_message
+mqttclient.connect(MQTT_Settings['Server'], MQTT_Settings['Port'], 60)
+
+
+# Blocking call that processes network traffic, dispatches callbacks and handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a manual interface.
+mqttclient.loop_forever()
+
+

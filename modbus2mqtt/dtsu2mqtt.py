f73983b95f959e3872b1e34fec5be4cc13283798
Rune Skou Larsen <user.email=1193502+runesl@users.noreply.github.com>
2025-04-17 11:39:42 +0200

    optimize and instrument reading and publishing

--- modbus2mqtt/dtsu2mqtt.py
+++ modbus2mqtt/dtsu2mqtt.py
@@ -27,6 +27,7 @@ client1.connect(broker, port)  # establish connection
 power_sent = None
 energy_sent = None
 power_regs = [
+	"Total_System_Active_Power",
 	"Active_Power_L1",
 	"Active_Power_L2",
 	"Active_Power_L3"
@@ -41,26 +42,31 @@ energy_regs = [
 ]
 
 mra = ModbusReadahead(instrument)
-
+prev_update_tsmp = 0
+last_energy_tsmp = 0
+energy_delay = 1 # Only update energy each second to not delay time critical power readings
 while True:
 	try:
+		before_read_tsmp = time.time()
 		mra.read_float_ahead(Registermapping[power_regs[0]]["addr"], len(power_regs))
-		mra.read_float_ahead(Registermapping[energy_regs[0]]["addr"], len(energy_regs))
-
 		power = {reg: mra.read_float(Registermapping[reg]["addr"])*Registermapping[reg]["scale"] for reg in power_regs}
-		energy = {reg: mra.read_float(Registermapping[reg]["addr"])*Registermapping[reg]["scale"] for reg in energy_regs}
+		power["prev_update_tsmp"]=prev_update_tsmp
+		prev_update_tsmp = before_read_tsmp
 		if power != power_sent:
+			power["publish_tsmp"]=time.time()
 			client1.publish(f"smartmeter/power", json.dumps(power))
-			print(f"publish power: {power}")
+#			print(f"publish power: {power}")
 			power_sent = power
-		if energy != energy_sent:
-			client1.publish(f"smartmeter/energy", json.dumps(energy))
-			print(f"publish energy: {energy}")
-			energy_sent = energy
+		if time.time() > last_energy_tsmp + energy_delay:
+			last_energy_tsmp = time.time()
+			mra.read_float_ahead(Registermapping[energy_regs[0]]["addr"], len(energy_regs))
+			energy = {reg: mra.read_float(Registermapping[reg]["addr"])*Registermapping[reg]["scale"] for reg in energy_regs}
+			if energy != energy_sent:
+				client1.publish(f"smartmeter/energy", json.dumps(energy))
+#				print(f"publish energy: {energy}")
+				energy_sent = energy
 	except Exception as e:
 		print(f"Exception caught: {e}. Resetting serial modbus")
 		instrument.serial.close()
 		instrument.serial.open()
 		time.sleep(1)
-	time.sleep(0.05) # rs485usb com is more stable with this
-
